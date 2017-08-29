195429def5306c18edf4d97b63bf70de84fc06fe
Imran Jawaid <mac@macs-mbp.gateway.sasktel.net>
2017-08-28 23:54:26 -0600

    cleanup.

--- restApi/server.js
+++ restApi/server.js
@@ -6,9 +6,9 @@ var express = require('express'),
   bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:32769/todoList');
 mongoose.connect('mongodb://imranjaved728:fucklifE@ds163053.mlab.com:63053/todolist123');
-
+//mongoose.connect('mongodb://localhost:32769/todoList');
+//https://mlab.com/databases/todolist123
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
